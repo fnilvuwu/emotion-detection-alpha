3a95f878b946c3b97c8e21cb840dba4ed4e3d9d7
fnilvuwu <mamatmks45@gmail.com>
2022-12-08 23:34:00 +0800

    modified:   static/js/face-detection.js

--- static/js/face-detection.js
+++ static/js/face-detection.js
@@ -1,13 +1,10 @@
-// Initialize DOM selectors
 let warn = document.getElementById("model_log");
+// let enableWebcamButton = document.getElementById("webcamButton");
 let emotionText = document.getElementById("emotion");
 
-// Initialize emotions, and tflite model.
 const emotions = ["Angry", "Happy", "Sad", "Surprise"];
-let tfliteModel = undefined;
+var tfliteModel = undefined;
 
-// Load the (tflite) model.
-// Program would await until model is loaded before continuing.
 async function start() {
     await tflite.loadTFLiteModel(
         "static/model.tflite"
@@ -19,7 +16,6 @@ async function start() {
 }
 start();
 
-// Initialize OpenCV and webcams.
 function openCvReady() {
     cv['onRuntimeInitialized'] = () => {
         let video = document.getElementById("cam_input"); // video is the id of video tag
@@ -32,7 +28,6 @@ function openCvReady() {
                 console.log("An error occurred! " + err);
             });
 
-        // Initialize all neccesaries mats and utils.
         let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
         let dst = new cv.Mat(video.height, video.width, cv.CV_8UC4);
         let gray = new cv.Mat();
@@ -40,8 +35,6 @@ function openCvReady() {
         let cap = new cv.VideoCapture(cam_input);
         let faces = new cv.RectVector();
         let utils = new Utils('errorMessage');
-
-        // Load (haas) face classifier
         let classifier = new cv.CascadeClassifier();
         let faceCascadeFile = 'haarcascade_frontalface_default.xml';
         utils.createFileFromUrl(faceCascadeFile,
@@ -50,68 +43,68 @@ function openCvReady() {
             });
 
 
-        // FPS would determine how many frame per second.
         const FPS = 30;
+        // emotionText.classList.remove("invisible");
 
-        // Start processing and rendering the video
         function processVideo() {
-            // Save webcam data
             let begin = Date.now();
             cap.read(src);
             src.copyTo(dst);
             cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY, 0);
             
-            // Detect face(s) using previously intialized classifier.
+            // detect face(s)
             try {
                 classifier.detectMultiScale(gray, faces, 1.1, 3, 0);
             } catch (err) {
                 console.log(err);
             }
             
-            // For each faces detected, predict the emotions
             for (let i = 0; i < faces.size(); ++i) {
                 let face = faces.get(i);
                 let point1 = new cv.Point(face.x, face.y);
                 let point2 = new cv.Point(face.x + face.width, face.y + face.height);
                 
-                // Rect for gray roi
+                // rect for gray roi
                 let rect = new cv.Rect(face.x, face.y, face.width, face.height);
                 
-                // Mat for resize gray roi
+                // mat for resize gray roi
                 gray_roi = gray.roi(rect);
                 gray_roi_resize = new cv.Mat();
                 cv.resize(gray_roi, gray_roi_resize, new cv.Size(244, 244))
                 cv.imshow("canvas_roi", gray_roi_resize);
                 
-                // Predict using model
+                // predict using model
                 const outputTensor = tf.tidy(() => {
-                    // Transform the image data into Array pixels.
+                    // Get pixels data from an image.
                     let img = tf.browser.fromPixels(document.getElementById("canvas_roi"));
 
                     // Resize, normalize, expand dimensions of image pixels by 0 axis.:
                     img = tf.image.resizeBilinear(img, [48, 48]);
                     img = tf.div(tf.expandDims(img, 0), 255);
                     
-                    // Predict the emotions.
+                    // predict
                     let outputTensor = tfliteModel.predict(img);
                     return outputTensor;
                 });
 
-                // Convert to array and take prediction index with highest value
+                // convert to array and take prediction index with highest value
                 let output = outputTensor.arraySync();
                 let index = output[0].indexOf(Math.max(...output[0]));
                 
-                // Render rectangles and text
+                // render rectangles and text
                 cv.rectangle(dst, point1, point2, [255, 0, 0, 255], 2);
                 cv.putText(dst, emotions[index], new cv.Point(face.x, face.y),
                 cv.FONT_HERSHEY_SIMPLEX, 1, new cv.Scalar(0, 0, 255), 2);
                 cv.imshow("canvas_output", dst);
+                // emotionText.innerHTML(emotions[index]);
             }
-            // Schedule next frame.
+            
+            
+            // schedule next one.
             let delay = 1000 / FPS - (Date.now() - begin);
             setTimeout(processVideo, delay);
         }
-        // Schedule first frame
+
         setTimeout(processVideo, 0);
     };
 }
