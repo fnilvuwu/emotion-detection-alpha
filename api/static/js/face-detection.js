869aa751e0a6c04ece6b8d29777c70ad91bd0a10
Rahmatullah. R <50456560+fnilvuwu@users.noreply.github.com>
2022-12-09 00:14:24 +0800

    Update face-detection.js

--- api/static/js/face-detection.js
+++ api/static/js/face-detection.js
@@ -7,7 +7,7 @@ var tfliteModel = undefined;
 
 async function start() {
     await tflite.loadTFLiteModel(
-        "./static/model.tflite"
+        "api/static/model.tflite"
     ).then((loadedModel) => { 
         tfliteModel = loadedModel;
         warn.innerHTML = "Model has successfully loaded! Your camera should be displayed soon."
